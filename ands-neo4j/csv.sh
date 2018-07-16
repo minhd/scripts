9847a186140b1cc7f5d15bb1b77e84640a9611ac
Minh Duc Nguyen <minh.nguyen@ands.org.au>
2018-07-16 15:10:09 +1000

    Updated registry path

--- ands-neo4j/csv.sh
+++ ands-neo4j/csv.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 importPath=/Users/minhd/dev/ands/data/test/neo4j/import/
+registryHome=~/dev/ands/registry
 
 start=$(date +'%s')
 echo "exporting started... $importPath"
-cd ~/dev/ands/registry
+echo "registry: $registryHome"
+echo "import: $importPath"
+
+cd $registryHome
 for module in 'nodes' 'direct' 'relatedInfoNodes' 'relatedInfoRelations' 'identical' 'primary'
 do
    php ands.php export:csv --importPath=$importPath --$module -q &
